c0ee1dc50f7d4d80f80002ed2102d247192cd4bb
nicolaslopi <128952160+nicolaslopi@users.noreply.github.com>
2023-05-09 13:36:48 -0500

    se actualizaron los algoritmos de busqueda por amplitud y profundidad

--- src/gokusmartia/Nodo.java
+++ src/gokusmartia/Nodo.java
@@ -5,11 +5,13 @@ public class Nodo {
     int x;
     int y;  
     int level;
+    Nodo parent;
 
-    public Nodo(int x, int y, int level) {
+    public Nodo(int x, int y, int level, Nodo parent) {
         this.x = x;
         this.y = y;
         this.level = level;
+        this.parent = parent;
     }
 
     public int getX() {
@@ -35,4 +37,13 @@ public class Nodo {
     public void setLevel(int level) {
         this.level = level;
     }
+
+    public Nodo getParent() {
+        return parent;
+    }
+
+    public void setParent(Nodo parent) {
+        this.parent = parent;
+    }
+    
 }
