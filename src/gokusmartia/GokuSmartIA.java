7a2a7e0ae9a7e682b2ed80506c93b6f486cd5ed3
nicolaslopi <128952160+nicolaslopi@users.noreply.github.com>
2023-05-09 13:36:08 -0500

    se actualizaron los algoritmos de busqueda por amplitud y profundidad

--- src/gokusmartia/GokuSmartIA.java
+++ src/gokusmartia/GokuSmartIA.java
@@ -1,23 +1,41 @@
 
 package gokusmartia;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class GokuSmartIA {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int n = 5; // tamaño de la matriz
-        int m = 5;   
-        int esferaX = 0; // coordenadas de la esfera del dragón
-        int esferaY = 4;
+        int n = 10; // tamaño de la matriz
+        int m = 10;   
+        int esferaX = 4; // coordenadas de la esfera del dragón
+        int esferaY = 0;
+        /*
+            0 libre
+            1 muro
+            2 donde inicia goku
+            3 freezer
+            4 cell
+            5 semilla
+            6 esfera
+            */
         int[][] matrix = {
-            {1, 1, 1, 1, 1},
-            {1, 0, 0, 0, 1},
-            {1, 0, 0, 0, 1},
-            {1, 0, 0, 0, 1},
-            {1, 1, 1, 1, 1}
+            {0,5,3,1,1,1,1,1,1,1},
+            {0,1,0,0,1,0,0,0,1,1},
+            {0,1,1,0,3,5,1,0,2,0},
+            {0,1,1,1,3,1,1,1,1,0},
+            {6,0,0,0,0,0,0,0,0,0},
+            {1,1,4,1,1,1,1,1,1,0},
+            {1,1,0,4,4,0,0,1,1,5},
+            {1,1,0,0,1,1,0,1,1,0},
+            {0,0,0,0,1,1,5,0,0,0},
+            {1,1,1,6,1,1,0,1,1,1}
         };
+        
         //int[][] matriz = crearMatriz(n, m, esferaX, esferaY);
         System.out.println("Matriz:");
         for (int i = 0; i < n; i++) {
@@ -27,10 +45,11 @@ public class GokuSmartIA {
             System.out.println();
         }
         
-        System.out.println("Busqueda por amplitud");
+        
+        System.out.println("\nBUSQUEDA POR AMPLITUD\n");
         AlgoritmosDeBusquedaNoInformada.busquedaPorAmplitud(matrix, esferaX, esferaY);
-        System.out.println("\nBusqueda por profundidad");
-        AlgoritmosDeBusquedaNoInformada.busquedaPorProfundidad(matrix, esferaX, esferaY);
+        //System.out.println("\nBUSQUEDA POR PROFUNDIDAD\n");
+        //AlgoritmosDeBusquedaNoInformada.busquedaPorProfundidad(matrix, esferaX, esferaY);
     }
     
 }
